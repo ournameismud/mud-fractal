e3b14326b3ff3eae7f4d2877ae66f34f2f307d06
Dave Stockley <leon@ournameismud.co.uk>
2017-09-27 14:07:37 +0100

    udpate eslint conf

--- src/js/app.js
+++ src/js/app.js
@@ -14,13 +14,15 @@ new App(document, behaviours).start()
 /*
 * Register Service Worker
 */
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
-		.register('/sw.js')
-		.then(reg => {
-			log('Service Worker register', reg)
-		})
-		.catch(err => {
-			log('Service Worker error', err)
-		})
-}
+// if ('serviceWorker' in navigator) {
+// 	navigator.serviceWorker
+// 		.register('/sw.js')
+// 		.then(reg => {
+// 			log('Service Worker register', reg)
+// 		})
+// 		.catch(err => {
+// 			log('Service Worker error', err)
+// 		})
+// }
+
+thing()
