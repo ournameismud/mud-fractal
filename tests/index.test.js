e7f2485d4ab835c7a5dbc2b92ff12b1bd732a6c2
Dave Stockley <magicspon@gmail.com>
2018-03-19 23:16:06 +0000

    comment out test, remove sample file

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,24 +1,24 @@
-import Example from '../src/js/sample'
+// import Example from '../src/js/sample'
 
-describe('example', () => {
-	let example = new Example()
-	let value = example.value
+// describe('example', () => {
+// 	let example = new Example()
+// 	let value = example.value
 
-	document.body.innerHTML = '<div>' + '  <button id="btn" />' + '</div>'
+// 	document.body.innerHTML = '<div>' + '  <button id="btn" />' + '</div>'
 
-	it('should set the textContent of button to gary', () => {
-		example.addEvents()
+// 	it('should set the textContent of button to gary', () => {
+// 		example.addEvents()
 
-		const $node = document.getElementById('btn')
+// 		const $node = document.getElementById('btn')
 
-		$node.click()
+// 		$node.click()
 
-		expect($node.textContent).toBe('gary')
-	})
+// 		expect($node.textContent).toBe('gary')
+// 	})
 
-	it('should update the value property when some event is triggered', () => {
-		example.emit('some:event')
+// 	it('should update the value property when some event is triggered', () => {
+// 		example.emit('some:event')
 
-		expect(value).toBe(10)
-	})
-})
+// 		expect(value).toBe(10)
+// 	})
+// })
