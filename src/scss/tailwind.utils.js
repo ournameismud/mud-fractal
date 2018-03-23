4f565e88fefd3692ad0b050a13ec5a798cf83f68
Dave Stockley <magicspon@gmail.com>
2018-03-23 09:35:38 +0000

    change exports to module.exports

--- src/scss/tailwind.utils.js
+++ src/scss/tailwind.utils.js
@@ -1,54 +1,85 @@
-import ms from 'modularscale-js'
+const modularscale = require('modularscale-js')
 
-export const percentage = (x, y) => `${(x / y * 100).toFixed(4)}%`
+const percentage = (x, y) => `${(x / y * 100).toFixed(4)}%`
 
-export const widths = y => {
+module.exports = {
+	percentage,
+	widths,
+	rem,
+	em,
+	calc,
+	ms2rem,
+	ms,
+	gutter,
+	rgba,
+	rgbaRange,
+	lines,
+	msRange
+}
+
+const msSettings = {
+	base: 16,
+	ratio: 1.125
+}
+
+function widths(y) {
 	return Array.from({ length: y - 1 }).reduce((a, c, i) => {
 		a[`${i + 1}/${y}`] = percentage(i + 1, y)
 		return a
 	}, {})
 }
 
-export function rem(input, base = 16) {
+function stripUnits(str) {
+	const regExVal = new RegExp(/(-*(\d\.*)+)/, 'g')
+	return regExVal.exec(str)[0]
+}
+
+function rem(input, base = 16) {
 	const regExVal = new RegExp(/\d+/, 'g')
 	return regExVal.exec(input) / base + 'rem'
 }
 
-export function em(input, base = 16) {
-	const regExVal = new RegExp(/\d+/, 'g')
-	return regExVal.exec(input) / base + 'em'
+function em(input, base = 16) {
+	//const regExVal = new RegExp(/\d+/, 'g')
+	return input / base + 'em'
 }
 
-export function calc(min, max, start, stop) {
-	return `calc(${min} + (${max} - ${min}) * ((100vw - ${start}) / (${stop} - ${start})))`
+function calc(min, max, start, stop) {
+	console.log(min, max)
+
+	return `calc(${min} + (${stripUnits(max)} - ${stripUnits(
+		min
+	)}) * ((100vw - ${start}px) / (${stop} - ${start})))`
 }
 
-export const ms2rem = (
-	n,
-	settings = {
-		base: 16,
-		ratio: 1.13
-	}
-) => rem(ms(n, settings))
+function ms2rem(n, settings = msSettings) {
+	return rem(modularscale(n, settings))
+}
 
-export function msRange(
+function ms(n, settings = msSettings) {
+	return modularscale(n, settings)
+}
+
+function msRange(
 	start,
 	end,
 	settings = {
 		base: 16,
-		ratio: 1.13
+		ratio: 1.125
 	}
 ) {
 	const length = end + 1 - start
 	return Array.from({ length: length }).reduce((acc, cur, i) => {
-		acc[`ms${start + i}`] = rem(ms(start + i, settings))
+		acc[`ms${start + i}`] = rem(modularscale(start + i, settings))
 		return acc
 	}, {})
 }
 
-export const gutter = (n = 1, size = 20) => rem(n * size)
+function gutter(n = 1, size = 20) {
+	return rem(n * size)
+}
 
-export function rgba(hex, opacity = 1) {
+function rgba(hex, opacity = 1) {
 	const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i
 	const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
 		hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b)
@@ -62,11 +93,19 @@ export function rgba(hex, opacity = 1) {
 		: null
 }
 
-export function rgbaRange(hex, name = 'black', prefix = 'pc') {
+function rgbaRange(hex, name = 'black', prefix = 'pc') {
 	return Array.from({ length: 9 })
 		.map((_, i) => rgba(hex, (i + 1) / 10))
 		.reduce((a, c, i) => {
 			a[`${name}-${(i + 1) * 10}${prefix}`] = c
 			return a
 		}, {})
+}
+
+function lines(start, end, { grid = 8, fs = 18 }) {
+	const length = end + 1 - start
+	return Array.from({ length: length }).reduce((acc, cur, i) => {
+		acc[`${i}`] = i * grid / fs
+		return acc
+	}, {})
 }
