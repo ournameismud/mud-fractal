594075b3c0bc7b511be46b976143f96e4acc4a8f
Dave Stockley <magicspon@gmail.com>
2018-05-20 20:35:12 +0100

    added eventBus tests

---
+++ __tests__/eventBus.test.js
@@ -0,0 +1,24 @@
+import eventBus from '@/core/eventBus'
+
+describe('loader function', () => {
+	it('should be a function', () => {
+		expect(eventBus).toBeInstanceOf(Object)
+	})
+
+	it('should return 3 methods [emit, on, off]', () => {
+		expect(eventBus.emit).toBeInstanceOf(Function)
+		expect(eventBus.on).toBeInstanceOf(Function)
+		expect(eventBus.off).toBeInstanceOf(Function)
+	})
+
+	it('should respone to emmited events', async () => {
+		let temp = 0
+
+		const fn = () => (temp += 1)
+		eventBus.on('some:funk', fn)
+		eventBus.emit('some:funk')
+		eventBus.off('some:funk', fn)
+		eventBus.emit('some:funk')
+		expect(temp).toBe(1)
+	})
+})
